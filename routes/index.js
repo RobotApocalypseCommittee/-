689700f4414aad354f6ecc63f35145ce63d98763
ObjectionTheory <therandomguy265@gmail.com>
2018-01-31 20:47:00 +0000

    Testing some βεκος

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var player = require('play-sound')(opts = {})
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'βεκος' });
 });
+
+var playSound = player.play('../alert.mp3', { timeout: 300 }, function(err){
+  if (err) throw err
+})
 
 module.exports = router;
