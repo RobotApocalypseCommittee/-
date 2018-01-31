be0b54625d0ec53b6bb28b366e356dffd2d7ca15
ObjectionTheory <therandomguy265@gmail.com>
2018-01-31 22:23:53 +0000

    I don;t even βεκος anymore

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,6 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'βεκος' });
 });
 
-var playSound = player.play('../alert.mp3', { timeout: 300 }, function(err){
-  if (err) throw err
-})
+
 
 module.exports = router;
