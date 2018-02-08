2905f2478b7d69750b611a27f23126cb397e7fde
Joe <joe.bell34@btinternet.com>
2018-02-08 20:45:01 +0000

    Hopeful...

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,15 @@ var gulp = require('gulp');
 var minifyCSS = require('gulp-csso');
 var postcss      = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
 
 
 gulp.task('css', function(){
     return gulp.src('./src/**/*.css')
+        .pipe(sourcemaps.init())
         .pipe(postcss([ autoprefixer() ]))
         .pipe(minifyCSS())
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('./public'))
 });
 
