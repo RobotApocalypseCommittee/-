dcf90c79acdc9d61b1490ac615bf273be3265719
Joe <joe.bell34@btinternet.com>
2018-02-08 23:05:56 +0000

    Nicee

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,16 @@ var minifyCSS = require('gulp-csso');
 var postcss      = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
+var nodemon = require("gulp-nodemon");
 
+gulp.task('develop', ['build'], function () {
+    var stream = nodemon({ 
+        script: 'app.js',
+        ext: "css",
+        ignore: ['public/*'],
+        tasks: ['build'] })
+
+    })
 
 gulp.task('css', function(){
     return gulp.src('./src/**/*.css')
@@ -14,4 +23,6 @@ gulp.task('css', function(){
         .pipe(gulp.dest('./public'))
 });
 
-gulp.task('default', [ 'css' ]);
+gulp.task("build", [ "css"]);
+
+gulp.task('default', [ 'build' ]);
